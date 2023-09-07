c6357ea84af39f3ba83e573a6fb2a293e18b9710
墨涤千尘 <leey319@163.com>
2023-09-07 17:40:56 +0800

    修复 forge 一个 mixin 错误

--- common/src/main/java/me/langyue/autotranslation/mixin/GuiGraphicsMixin.java
+++ common/src/main/java/me/langyue/autotranslation/mixin/GuiGraphicsMixin.java
@@ -6,15 +6,14 @@ import me.langyue.autotranslation.translate.TranslatorManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -24,9 +23,9 @@ import java.util.Optional;
 @Mixin(GuiGraphics.class)
 public class GuiGraphicsMixin {
 
-    @Redirect(method = "drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Font;drawInBatch(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/client/gui/Font$DisplayMode;IIZ)I"))
-    private int drawStringMixin(Font instance, String string, float f, float g, int i, boolean bl, Matrix4f matrix4f, MultiBufferSource multiBufferSource, Font.DisplayMode displayMode, int j, int k, boolean bl2) {
-        return instance.drawInBatch(autoTranslation$getTranslate(string), f, g, i, bl, matrix4f, multiBufferSource, displayMode, j, k, bl2);
+    @ModifyVariable(method = "drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private String drawStringMixin(String string) {
+        return autoTranslation$getTranslate(string);
     }
 
     @Redirect(method = "drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIIZ)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;getVisualOrderText()Lnet/minecraft/util/FormattedCharSequence;"))
